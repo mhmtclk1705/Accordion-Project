78e3467e29da5150d3b35cc7008940730d651a7a
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-04-02 01:09:24 +0300

    comment

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import data from "./data";
 import SingleQuestion from "./Question";
 function App() {
   const [questions, setQuestions] = useState(data);
+  // datayı kullanma işlemi
 
   return (
     <main>
+      {/*  style={{display:"flex"}} */}
       <div className="container">
+        {/*  style={{width:"30%"}} */}
         <h3>questions and answers about login</h3>
 
         <section className="info">
+          {/* map ile data içerisinden id yi alıyoruz geri kalanını rest ile props kullanarak singlequestion
+          componentine gönderiyoruz */}
           {questions.map((question) => {
             return <SingleQuestion key={question.id} {...question} />;
           })}
--- src/Question.js
+++ src/Question.js
@@ -7,10 +7,13 @@ const Question = ({ title, info }) => {
     <article className="question">
       <header>
         <h4>{title}</h4>
+        {/* click yönteminde setShow ile show false ise true true ise false olması için  !showInfo kullanıyoruz */}
         <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {/* eğer show info true ise - false ise + iconu gözükmesi için  */}
           {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
+      {/* show infonun true olduğu durumda collapse açılsın ve p tagi içerisindeki info gözüksün istiyoruz */}
       {showInfo && <p>{info}</p>}
     </article>
   );
